aa3c706427c5505a5d8687a08b5a275267b9a557
cchromak <cchromak@gmail.com>
2020-07-30 17:30:15 -0400

    Formatted drawing options

--- script.js
+++ script.js
@@ -38,10 +38,10 @@ function moveUp(){
 
 function moveDown(){
     var top = parseFloat(window.getComputedStyle(pen).getPropertyValue("top"));
-    if (top < 545 && count > 0) {
+    if (top < 565 && count > 0) {
         pen.style.top = top + .1 + "px";
         count--;
-    } else if (top < 545) {
+    } else if (top < 565) {
         pen.style.top = top + .5 + "px";
     }
 }
